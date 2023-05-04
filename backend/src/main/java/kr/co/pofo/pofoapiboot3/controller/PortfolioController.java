d4b0c17944de93a15bcf7edcc7dee54f94ca1495
mjhn010 <mjhn010@naver.com>
2023-05-04 16:27:06 +0900

    Add fetch api

--- backend/src/main/java/kr/co/pofo/pofoapiboot3/controller/PortfolioController.java
+++ backend/src/main/java/kr/co/pofo/pofoapiboot3/controller/PortfolioController.java
@@ -8,12 +8,12 @@ import kr.co.pofo.pofoapiboot3.service.PortfolioContentsService;
 import kr.co.pofo.pofoapiboot3.service.PortfolioService;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/pofo")
 public class PortfolioController {
     private final PortfolioService portfolioService;
@@ -29,13 +29,13 @@ public class PortfolioController {
     @GetMapping("{portfolioId}")
     public Map<String, Object> getPortfolioDetail(@PathVariable("portfolioId") Integer portfolioId) {
         Portfolio portfolio = portfolioService.getPortfolioById(portfolioId);
-        List<PortfolioContents> portfolioContents = portfolioContentsService.getPortfolioContents(portfolioId);
+        List<PortfolioContents> contents = portfolioContentsService.getPortfolioContents(portfolioId);
         List<Comment> comments = commentService.getCommentsByPortfolioId(portfolioId);
 
         Map<String, Object> objectMap = new LinkedHashMap<>();
 
         objectMap.put("portfolio", portfolio);
-        objectMap.put("portfolioContents", portfolioContents);
+        objectMap.put("contents", contents);
         objectMap.put("comments", comments);
 
         return objectMap;
