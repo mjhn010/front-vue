95b8c404c2d68022e3b73715b56ae583f4d0feaa
shl811 <suhyeonlee811@gmail.com>
2023-05-09 12:24:10 +0900

    corsconfig 이름 수정

--- backend/src/main/java/kr/co/pofo/pofoapiboot3/config/CorsConfig.java
+++ backend/src/main/java/kr/co/pofo/pofoapiboot3/config/CorsConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CORSConfig {
     
     @Bean
     WebMvcConfigurer webMvcConfigurer(){
