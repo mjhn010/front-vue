91cfcd5a5747299ecd3be7dcf0b72449c151ab1e
hongseongje <kuoo123@naver.com>
2023-05-11 11:48:02 +0900

    goot

---
+++ frontend/src/components/LoginModal.vue
@@ -0,0 +1,23 @@
+
+<style scoped>
+
+
+
+@import url("/src/assets/css/compoment/loginmodal.css");
+</style>
+<template>
+   <div class="black-bg">
+        <div class="white-bg">
+            <div class="이미지가운데">
+                <img src="../assets/images/느낌표.png" class="느낌표">
+            </div>
+            <div class="flex-colum line-text">
+                <h5> 로그인이 필요한 기능입니다</h5>
+            </div>
+            <div class="flex-row-between1 margin-top-20">
+                <button class="check-btn">확인</button>
+                <button class="btn-back" >취소</button>
+            </div>
+        </div>
+    </div>
+</template>
