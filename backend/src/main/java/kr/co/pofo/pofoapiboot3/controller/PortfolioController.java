624bef3f4baf3d7935c3245caeadd79796155ab2
Choi Eunchang <contact@eunchang.me>
2023-05-12 12:57:13 +0900

    Refactor portfolio controller

--- backend/src/main/java/kr/co/pofo/pofoapiboot3/controller/PortfolioController.java
+++ backend/src/main/java/kr/co/pofo/pofoapiboot3/controller/PortfolioController.java
@@ -1,47 +1,37 @@
 package kr.co.pofo.pofoapiboot3.controller;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import kr.co.pofo.pofoapiboot3.entity.CommentView;
 import kr.co.pofo.pofoapiboot3.entity.Portfolio;
 import kr.co.pofo.pofoapiboot3.entity.PortfolioContents;
 import kr.co.pofo.pofoapiboot3.service.CommentService;
 import kr.co.pofo.pofoapiboot3.service.PortfolioContentsService;
 import kr.co.pofo.pofoapiboot3.service.PortfolioService;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 @CrossOrigin
+@RestController
 @RequestMapping("/pofo")
 public class PortfolioController {
     private final PortfolioService portfolioService;
     private final PortfolioContentsService portfolioContentsService;
-    private final CommentService commentService;
 
     public PortfolioController(PortfolioService portfolioService, PortfolioContentsService portfolioContentsService, CommentService commentService) {
         this.portfolioService = portfolioService;
         this.portfolioContentsService = portfolioContentsService;
-        this.commentService = commentService;
     }
 
     @GetMapping("{portfolioId}")
     public Map<String, Object> getPortfolioDetail(@PathVariable("portfolioId") Integer portfolioId) {
         Portfolio portfolio = portfolioService.getPortfolioById(portfolioId);
         List<PortfolioContents> contents = portfolioContentsService.getPortfolioContents(portfolioId);
-        List<CommentView> comments = commentService.getCommentViews(portfolioId);
 
         Map<String, Object> objectMap = new LinkedHashMap<>();
 
         objectMap.put("portfolio", portfolio);
         objectMap.put("contents", contents);
-        objectMap.put("comments", comments);
 
         return objectMap;
     }
