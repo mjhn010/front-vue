6edc6cb1090c76793982cb60314c6916d5031be2
mjhn010 <mjhn010@naver.com>
2023-05-12 11:07:53 +0900

    member/reg

--- frontend/src/components/member/route.js
+++ frontend/src/components/member/route.js
@@ -1,9 +1,11 @@
 import { useUserDetailsStore } from "../../stores/useUserDetailsStore.js";
 import Mypage from './Mypage.vue';
+import PofoReg from './PortfolioRegister.vue'
 const member = {
   path: "/member", 
   children :[
-    { path : "mypage", component : Mypage }
+    { path : "mypage", component : Mypage },
+    { path : "reg", component : PofoReg }
   ],
   beforeEnter(to,from,next) {
     let userDetails = useUserDetailsStore();
