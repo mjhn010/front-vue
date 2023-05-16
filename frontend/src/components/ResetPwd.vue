a76b194cb443272a2015e032cbeb174447cdabc5
GuGuda <whddn35789@gmail.com>
2023-05-16 11:13:37 +0900

    sendMail & pwdFind Front Commit

---
+++ frontend/src/components/ResetPwd.vue
@@ -0,0 +1,69 @@
+<template>
+  <!DOCTYPE html>
+  <html xmlns:th="http://www.thymeleaf.org">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Document</title>
+    </head>
+
+    <body>
+      <section class="main">
+        <main class="reset-border">
+          <h1 class="margin-top-9">
+            <img src="/src/assets/images/pofo.svg" class="logo-img" />
+          </h1>
+          <h2 class="margin-top-9">비밀번호 변경</h2>
+
+          <form id="reset-form">
+            <div class="margin-top-9">
+              <input
+                class="input-text input-text-placeholder"
+                type="password"
+                id="pasword"
+                placeholder="비밀번호(숫자,영문,특수문자 포함 8~16자리)"
+                name="pwd"
+                autocomplete="off"
+              />
+              <span id="pwd-validation" class="block-red margin-top-2"></span>
+            </div>
+
+            <input
+              class="input-text input-text-placeholder margin-top-8"
+              type="password"
+              id="password-check"
+              placeholder="비밀번호 확인"
+              autocomplete="off"
+            />
+            <span id="pwd-certification" class="block-red margin-top-2"></span>
+
+            <input
+              class="margin-top-10 btn btn-1"
+              type="button"
+              value="비밀번호 변경"
+              id="send-btn"
+            />
+          </form>
+
+          <div class="modal-bg d-none">
+            <section class="modal-style d-none">
+              <div class="margin-top-10">
+                <span id="modal-text"></span>
+              </div>
+              <button class="btn btn-3" type="button" id="cancel">확인</button>
+            </section>
+          </div>
+        </main>
+      </section>
+    </body>
+  </html>
+</template>
+
+<style scoped>
+@import url("/src/assets/css/compoment/pwd-page.css");
+@import url("/src/assets/css/common/reset.css");
+@import url("/src/assets/css/common/style.css");
+@import url("/src/assets/css/common/util.css");
+@import url("/src/assets/css/common/buttons.css");
+</style>
