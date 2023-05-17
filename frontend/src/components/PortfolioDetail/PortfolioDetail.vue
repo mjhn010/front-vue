4597e12de40c80f9c3906b7a1c88fbb1b227a699
Choi Eunchang <contact@eunchang.me>
2023-05-17 17:08:44 +0900

    Fix router link

--- frontend/src/components/PortfolioDetail/PortfolioDetail.vue
+++ frontend/src/components/PortfolioDetail/PortfolioDetail.vue
@@ -45,6 +45,7 @@ function copyLink() {
   navigator.clipboard.writeText(url);
   alert("링크가 복사되었습니다.");
 }
+
 function scrollToTop() {
   window.scrollTo(0, 0);
 }
@@ -72,17 +73,17 @@ function scrollRight() {
 }
 
 function toggleLike() {
-    if (!useUserDetailsStore().id) {
-        return alert("로그인 후 이용해주세요.");
-    }
+  if (!useUserDetailsStore().id) {
+    return alert("로그인 후 이용해주세요.");
+  }
 
-    if (!state.onLiked) {
-        saveLike();
-        return (state.onLiked = true);
-    } else {
-        deleteLikes();
-        return (state.onLiked = false);
-    }
+  if (!state.onLiked) {
+    saveLike();
+    return (state.onLiked = true);
+  } else {
+    deleteLikes();
+    return (state.onLiked = false);
+  }
 }
 
 // Get data
@@ -230,8 +231,7 @@ onMounted(getData);
 <template>
   <Header />
   <div
-    class="absolute min-h-full w-full gap-y-2 bg-gray-50 xl:grid xl:grid-cols-12
-    xl:px-16 xl:pb-8 xl:pt-12"
+    class="absolute min-h-full w-full gap-y-2 bg-gray-50 xl:grid xl:grid-cols-12 xl:px-16 xl:pb-8 xl:pt-12"
   >
     <div
       class="w-full bg-white pb-4 xl:ml-36 xl:rounded-lg xl:border"
@@ -245,7 +245,7 @@ onMounted(getData);
             :src="`http://localhost:8080/profileImage/${state.member.image}`"
             alt="Profile image"
             @click="scrollToTop"
-          >
+          />
         </router-link>
 
         <figcaption class="flex cursor-default flex-col justify-evenly">
@@ -264,17 +264,15 @@ onMounted(getData);
             <span
               class="cursor-pointer text-xs font-semibold hover:text-gray-500 sm:text-lg"
               @click="toggleCommentBox"
-            >팔로우</span>
+              >팔로우</span
+            >
           </div>
         </figcaption>
       </figure>
 
       <!-- Main -->
       <main>
-        <template
-          :key="content.id"
-          v-for="content in state.contents"
-        >
+        <template :key="content.id" v-for="content in state.contents">
           <div v-html="contentToHTML(content)" />
         </template>
       </main>
@@ -291,8 +289,7 @@ onMounted(getData);
             :to="`/search?text=${skill.engName.toLowerCase()}`"
           >
             <div
-              class="block w-fit cursor-pointer rounded-full border px-3 py-1
-              font-semibold text-gray-600 hover:bg-blue-50"
+              class="block w-fit cursor-pointer rounded-full border px-3 py-1 font-semibold text-gray-600 hover:bg-blue-50"
               v-text="skill.engName"
             />
           </router-link>
@@ -305,7 +302,7 @@ onMounted(getData);
             class="h-6 w-6"
             :key="copyright.name"
             v-for="copyright in portfolioCopyright"
-          >
+          />
         </div>
       </div>
       <!-- Banner -->
@@ -315,8 +312,7 @@ onMounted(getData);
       >
         <div class="flex w-32 justify-evenly">
           <div
-            class="mb-2 flex h-12 w-12 cursor-pointer flex-col items-center
-            justify-center rounded-full"
+            class="mb-2 flex h-12 w-12 cursor-pointer flex-col items-center justify-center rounded-full"
             :class="
               state.onLiked ? 'bg-gray-800 hover:bg-gray-900' : 'bg-white'
             "
@@ -337,14 +333,14 @@ onMounted(getData);
             />
           </div>
           <div
-            class="collection-icon mb-2 cursor-pointer rounded-full
-            border-2 bg-white hover:bg-blue-50"
+            class="collection-icon mb-2 cursor-pointer rounded-full border-2 bg-white hover:bg-blue-50"
           />
         </div>
         <span
           class="text-sm font-bold text-blue-300"
           v-if="state.portfolio.awardDate != null"
-        >POFO PICK 선정</span>
+          >POFO PICK 선정</span
+        >
         <span
           class="text-lg font-bold text-white sm:text-xl"
           v-text="state.portfolio.title"
@@ -358,10 +354,9 @@ onMounted(getData);
               .replace(/-/g, '.')} | 그래픽 디자인 · UI/UX`
           "
         />
-        <span
-          class="text-xs font-semibold text-white sm:text-sm"
-          v-else
-        >그래픽 디자인 · UI/UX</span>
+        <span class="text-xs font-semibold text-white sm:text-sm" v-else
+          >그래픽 디자인 · UI/UX</span
+        >
       </div>
 
       <!-- Member's portfolio list bar -->
@@ -372,12 +367,14 @@ onMounted(getData);
           @click="scrollToTop"
           class="flex items-center justify-end"
         >
-          <span class="block text-sm font-semibold text-gray-500">프로필 자세히 보기</span>
+          <span class="block text-sm font-semibold text-gray-500"
+            >프로필 자세히 보기</span
+          >
           <img
             src="/src/assets/images/chevron-right.svg"
             alt="Chevron right icon"
             class="h-4 w-4 opacity-50"
-          >
+          />
         </router-link>
       </div>
 
@@ -397,7 +394,7 @@ onMounted(getData);
                 :src="`/src/assets/images/temp/${memberPortfolio.thumbnail}`"
                 alt="#"
                 class="h-full w-72 rounded-t-lg"
-              >
+              />
               <figcaption
                 class="w-72 rounded-b-lg bg-gray-950 px-5 text-sm font-bold text-white"
                 v-text="memberPortfolio.title"
@@ -409,13 +406,11 @@ onMounted(getData);
         <!-- Scroll buttons -->
         <div class="relative bottom-40 flex h-0 w-full justify-between px-4">
           <div
-            class="chevron-left-icon cursor-pointer border bg-white shadow-lg
-            hover:bg-blue-50 hover:duration-300"
+            class="chevron-left-icon cursor-pointer border bg-white shadow-lg hover:bg-blue-50 hover:duration-300"
             @click="scrollLeft"
           />
           <div
-            class="chevron-right-icon col-start-12 cursor-pointer justify-self-center
-            border bg-white shadow-lg hover:bg-blue-50 hover:duration-300"
+            class="chevron-right-icon col-start-12 cursor-pointer justify-self-center border bg-white shadow-lg hover:bg-blue-50 hover:duration-300"
             @click="scrollRight"
           />
         </div>
@@ -437,7 +432,7 @@ onMounted(getData);
             class="mb-2 h-12 w-12 rounded-full border-2"
             src="/src/assets/images/temp/d.bronze.jpg"
             alt="Profile image"
-          >
+          />
         </router-link>
 
         <figcaption class="block text-center text-sm font-bold">
@@ -448,8 +443,7 @@ onMounted(getData);
         class="my-6 flex flex-col items-center text-center text-sm font-bold"
       >
         <div
-          class="mb-2 flex h-12 w-12 cursor-pointer flex-col items-center
-          justify-center rounded-full border-2"
+          class="mb-2 flex h-12 w-12 cursor-pointer flex-col items-center justify-center rounded-full border-2"
           :class="state.onLiked ? 'bg-gray-800 hover:bg-gray-900' : 'bg-white'"
           @click="toggleLike"
         >
@@ -473,17 +467,15 @@ onMounted(getData);
         class="my-6 flex flex-col items-center text-center text-sm font-bold"
       >
         <div
-          class="collection-icon mb-2 cursor-pointer rounded-full
-          border-2 bg-white duration-300 hover:bg-blue-50"
+          class="collection-icon mb-2 cursor-pointer rounded-full border-2 bg-white duration-300 hover:bg-blue-50"
         />
-        컬렉션
+        북마크
       </div>
       <div
         class="my-6 flex flex-col items-center text-center text-sm font-bold"
       >
         <div
-          class="comment-icon mb-2 cursor-pointer rounded-full border-2
-          bg-white duration-300 hover:bg-blue-50"
+          class="comment-icon mb-2 cursor-pointer rounded-full border-2 bg-white duration-300 hover:bg-blue-50"
           @click="toggleCommentBox"
         />
         댓글
@@ -492,8 +484,7 @@ onMounted(getData);
         class="my-6 flex flex-col items-center text-center text-sm font-bold"
       >
         <div
-          class="share-icon mb-2 cursor-pointer rounded-full border-2 bg-white
-          duration-300 hover:bg-blue-50"
+          class="share-icon mb-2 cursor-pointer rounded-full border-2 bg-white duration-300 hover:bg-blue-50"
           @click="copyLink"
         />
         공유하기
@@ -502,10 +493,9 @@ onMounted(getData);
         class="my-6 flex flex-col items-center text-center text-sm font-bold"
       >
         <div
-          class="mb-2 flex h-12 w-12 cursor-pointer items-center justify-center
-          rounded-full border-2 bg-white hover:bg-blue-50 duration-300"
+          class="mb-2 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full border-2 bg-white duration-300 hover:bg-blue-50"
         >
-          <div class="h-8 w-8 bg-fire" />
+          <div class="h-7 w-7 bg-fire" />
         </div>
         신고하기
       </div>
@@ -531,8 +521,7 @@ onMounted(getData);
           </span>
         </div>
         <div
-          class="mb-2 flex h-12 w-12 cursor-pointer flex-col items-center
-          justify-center rounded-full border-2"
+          class="mb-2 flex h-12 w-12 cursor-pointer flex-col items-center justify-center rounded-full border-2"
           :class="state.onLiked ? 'bg-gray-800 hover:bg-gray-900' : 'bg-white'"
           @click="toggleLike"
         >
@@ -558,20 +547,17 @@ onMounted(getData);
         />
         <textarea
           id="comment-input"
-          class="col-span-7 mb-5 h-36 min-w-fit resize-none rounded-lg
-          border border-black px-5 py-3 text-sm font-normal"
+          class="col-span-7 mb-5 h-36 min-w-fit resize-none rounded-lg border border-black px-5 py-3 text-sm font-normal"
           placeholder="이 작업에 대한 댓글을 남겨주세요."
         />
         <button
-          class="col-span-2 col-start-5 mr-1 flex h-9 items-center
-          justify-center rounded-full border text-center text-sm font-semibold"
+          class="col-span-2 col-start-5 mr-1 flex h-9 items-center justify-center rounded-full border text-center text-sm font-semibold"
           @click="saveComment"
         >
           댓글 작성
         </button>
         <div
-          class="col-start-7 flex cursor-pointer items-center justify-center
-          rounded-full border text-sm font-semibold"
+          class="col-start-7 flex cursor-pointer items-center justify-center rounded-full border text-sm font-semibold"
         >
           취소
         </div>
@@ -585,29 +571,25 @@ onMounted(getData);
       >
         <div class="grid grid-cols-7 grid-rows-2">
           <figure class="col-span-7 grid grid-cols-6 grid-rows-2">
-            <a
-              href="#"
-              class="row-span-2"
-            >
+            <router-link class="h-0" :to="`/profile/${comment.memberId}`">
               <img
                 class="h-12 w-12 rounded-full"
                 :src="`/src/assets/images/temp/${comment.memberImage}`"
                 alt="Profile image"
-              >
-            </a>
-            <div
-              class="col-start-2 font-bold"
-              v-text="comment.memberNickname"
-            />
+              />
+            </router-link>
+            <router-link class="h-0" :to="`/profile/${comment.memberId}`">
+              <div
+                class="col-start-2 font-bold hover:text-gray-600"
+                v-text="comment.memberNickname"
+              />
+            </router-link>
             <div
               class="col-start-2 text-xs font-semibold text-gray-500"
               v-text="comment.regDate.substring(0, 10).replace(/-/g, '.')"
             />
           </figure>
-          <p
-            class="col-span-7 my-4 text-sm"
-            v-text="comment.content"
-          />
+          <p class="col-span-7 my-4 text-sm" v-text="comment.content" />
           <div
             class="col-span-2 cursor-pointer text-start text-xs text-gray-500"
           >
