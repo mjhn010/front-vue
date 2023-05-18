bf6d2af7bb995f34bb98df2bd63a2b10ae77317f
Choi Eunchang <contact@eunchang.me>
2023-05-18 10:41:51 +0900

    Delete dto

--- backend/src/main/java/kr/co/pofo/pofoapiboot3/entity/BookmarkDto.java
+++
@@ -1,9 +0,0 @@
-package kr.co.pofo.pofoapiboot3.entity;
-
-import lombok.Data;
-
-@Data
-public class BookmarkDto {
-    private int portfolioId;
-    private int memberId;
-}
--- backend/src/main/java/kr/co/pofo/pofoapiboot3/entity/ReportDto.java
+++
@@ -1,9 +0,0 @@
-package kr.co.pofo.pofoapiboot3.entity;
-
-import lombok.Data;
-
-@Data
-public class ReportDto {
-    private Integer memberId;
-    private String url;
-}
