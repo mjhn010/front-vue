b8b7a8d80fd2321914cb13b69ec4e92b42523594
hongseongje <kuoo123@naver.com>
2023-05-19 18:44:50 +0900

    마이 페이지 수정 중

--- frontend/src/components/FollowModal.vue
+++ frontend/src/components/FollowModal.vue
@@ -1,142 +1,27 @@
-<script setup>
-function
-
-</script>
-<style scoped>
- @import url("/src/assets/css/compoment/followmodal.css");
-
-</style>
 <template>
-     <div class="black-bg">
+
+<div class="black-bg">
         <div class="white-bg">
             <div>
-                <h3 class="flex-row-between1">팔로워(4)<img src="/common/img/엑스.png" class="엑스"></h3>
+                <h3 class="flex-row-between1">팔로워(4)<img class="엑스" src="/src/assets/images/xMark.png"/></h3>
             </div>
             <div class="follower">
-                        <!--        팔로우하면 생김       -->
-                    <div class="line margin-top-2 flex-row-between1"></div>
-                    
-                        <div class="flex-row-between1">
-                          <img class="profile-img margin-top-2 " src="/common/img/portfolio32.jpg">
-                                 <div class="flex-row-between1">
-                                      <h4 class="margin-left-5 margin-top-6">림계진</h4>
-                                        <button class="follow-btn margin-top-4">+ 팔로우</button>
-                                 </div>
-                         </div>
-                    <!--              여기까지                  -->
+                <!--팔로우하면 생김-->
+                <div class="line margin-top-2 flex-row-between1"></div>
 
-                      <!--        팔로우하면 생김       -->
-                      <div class="line margin-top-2 flex-row-between1"></div>
-                    
-                      <div class="flex-row-between1">
-                        <img class="profile-img margin-top-2 " src="/common/img/portfolio32.jpg">
-                               <div class="flex-row-between1">
-                                    <h4 class="margin-left-5 margin-top-6">림계진</h4>
-                                      <button class="follow-btn margin-top-4">+ 팔로우</button>
-                               </div>
-                       </div>
-                  <!--              여기까지                  -->
-
-                    <!--        팔로우하면 생김       -->
-                    <div class="line margin-top-2 flex-row-between1"></div>
-                    
-                        <div class="flex-row-between1">
-                          <img class="profile-img margin-top-2 " src="/common/img/portfolio32.jpg">
-                                 <div class="flex-row-between1">
-                                      <h4 class="margin-left-5 margin-top-6">림계진</h4>
-                                        <button class="follow-btn margin-top-4">+ 팔로우</button>
-                                 </div>
-                         </div>
-                    <!--              여기까지                  -->
-                       <!--        팔로우하면 생김       -->
-                       <div class="line margin-top-2 flex-row-between1"></div>
-                    
-                       <div class="flex-row-between1">
-                         <img class="profile-img margin-top-2 " src="/common/img/portfolio32.jpg">
-                                <div class="flex-row-between1">
-                                     <h4 class="margin-left-5 margin-top-6">림계진</h4>
-                                       <button class="follow-btn margin-top-4">+ 팔로우</button>
-                                </div>
-                        </div>
-                   <!--              여기까지                  -->
-                      <!--        팔로우하면 생김       -->
-                      <div class="line margin-top-2 flex-row-between1"></div>
-                    
-                      <div class="flex-row-between1">
-                        <img class="profile-img margin-top-2 " src="/common/img/portfolio32.jpg">
-                               <div class="flex-row-between1">
-                                    <h4 class="margin-left-5 margin-top-6">림계진</h4>
-                                      <button class="follow-btn margin-top-4">+ 팔로우</button>
-                               </div>
-                       </div>
-                  <!--              여기까지                  -->
-                     <!--        팔로우하면 생김       -->
-                     <div class="line margin-top-2 flex-row-between1"></div>
-                    
-                     <div class="flex-row-between1">
-                       <img class="profile-img margin-top-2 " src="/common/img/portfolio32.jpg">
-                              <div class="flex-row-between1">
-                                   <h4 class="margin-left-5 margin-top-6">림계진</h4>
-                                     <button class="follow-btn margin-top-4">+ 팔로우</button>
-                              </div>
-                      </div>
-                 <!--              여기까지                  -->
-                    <!--        팔로우하면 생김       -->
-                    <div class="line margin-top-2 flex-row-between1"></div>
-                    
-                        <div class="flex-row-between1">
-                          <img class="profile-img margin-top-2 " src="/common/img/portfolio32.jpg">
-                                 <div class="flex-row-between1">
-                                      <h4 class="margin-left-5 margin-top-6">림계진</h4>
-                                        <button class="follow-btn margin-top-4">+ 팔로우</button>
-                                 </div>
-                         </div>
-                    <!--              여기까지                  -->
-                       <!--        팔로우하면 생김       -->
-                       <div class="line margin-top-2 flex-row-between1"></div>
-                    
-                       <div class="flex-row-between1">
-                         <img class="profile-img margin-top-2 " src="/common/img/portfolio32.jpg">
-                                <div class="flex-row-between1">
-                                     <h4 class="margin-left-5 margin-top-6">림계진</h4>
-                                       <button class="follow-btn margin-top-4">+ 팔로우</button>
-                                </div>
-                        </div>
-                   <!--              여기까지                  -->
-                      <!--        팔로우하면 생김       -->
-                      <div class="line margin-top-2 flex-row-between1"></div>
-                    
-                      <div class="flex-row-between1">
-                        <img class="profile-img margin-top-2 " src="/common/img/portfolio32.jpg">
-                               <div class="flex-row-between1">
-                                    <h4 class="margin-left-5 margin-top-6">림계진</h4>
-                                      <button class="follow-btn margin-top-4">+ 팔로우</button>
-                               </div>
-                       </div>
-                  <!--              여기까지                  -->
-                     <!--        팔로우하면 생김       -->
-                     <div class="line margin-top-2 flex-row-between1"></div>
-                    
-                     <div class="flex-row-between1">
-                       <img class="profile-img margin-top-2 " src="/common/img/portfolio32.jpg">
-                              <div class="flex-row-between1">
-                                   <h4 class="margin-left-5 margin-top-6">림계진</h4>
-                                     <button class="follow-btn margin-top-4">+ 팔로우</button>
-                              </div>
-                      </div>
-                 <!--              여기까지                  -->
-                    <!--        팔로우하면 생김       -->
-                    <div class="line margin-top-2 flex-row-between1"></div>
-                    
-                        <div class="flex-row-between1">
-                          <img class="profile-img margin-top-2 " src="/common/img/portfolio32.jpg">
-                                 <div class="flex-row-between1">
-                                      <h4 class="margin-left-5 margin-top-6">림계진</h4>
-                                        <button class="follow-btn margin-top-4">+ 팔로우</button>
-                                 </div>
-                         </div>
-                    <!--              여기까지                  -->
-        </div> 
+                <div class="flex-row-between1">
+                    <img class="profile-img margin-top-2 " src="/src/assets/images/portfolio32.jpg">
+                    <div class="flex-row-between1">
+                        <h4 class="margin-left-5 margin-top-6">림계진</h4>
+                        <button class="follow-btn margin-top-4">+ 팔로우</button>
+                    </div>
+                </div>
+                <!--여기까지-->
+            </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+    @import url("/src/assets/css/compoment/followmodal.css");
+</style>
