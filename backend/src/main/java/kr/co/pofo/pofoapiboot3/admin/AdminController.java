93a8fea817906a4e84d6776bd97fb419d00afc02
GuGuda <whddn35789@gmail.com>
2023-05-24 12:30:02 +0900

    dashboardCommit2

--- backend/src/main/java/kr/co/pofo/pofoapiboot3/admin/AdminController.java
+++ backend/src/main/java/kr/co/pofo/pofoapiboot3/admin/AdminController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin1")
+@RequestMapping("/dashboard")
 public class AdminController {
 
   @Autowired
--- backend/src/main/java/kr/co/pofo/pofoapiboot3/config/PofoSecurityConfig.java
+++ backend/src/main/java/kr/co/pofo/pofoapiboot3/config/PofoSecurityConfig.java
@@ -1,41 +1,61 @@
 package kr.co.pofo.pofoapiboot3.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@RequiredArgsConstructor
+@EnableWebSecurity
 @Configuration
-public class PofoSecurityConfig {
-//docs.spring.io/spring-security/reference/servlet/authorization/expression-based.html
-//www.baeldung.com/spring-security-thymeleaf
-   
-@Bean
-public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+public class PofoSecurityConfig implements WebMvcConfigurer {
 
-   http.csrf().disable()
-   .authorizeHttpRequests(authorize -> authorize
-       .requestMatchers("/admin/**").hasRole("ADMIN")
-       .requestMatchers("/member/**").hasAnyRole("ADMIN", "MEMBER")
-       .anyRequest().permitAll()
-   )
-   .formLogin()
-       .loginPage("/user/login") //GET
-       .loginProcessingUrl("/user/login") //post
-       .defaultSuccessUrl("/index")
-   .and()
-           .logout()
-           .logoutUrl("/user/logout")
-           .logoutSuccessUrl("/");
+  //docs.spring.io/spring-security/reference/servlet/authorization/expression-based.html
+  //www.baeldung.com/spring-security-thymeleaf
 
-       return http.build();
-   }
+  private final SingleVisitInterceptor singleVisitInterceptor;
 
-   
-   @Bean
-   public PasswordEncoder passwordEncoder() {
-      return new BCryptPasswordEncoder();      
-   }
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+      .csrf()
+      .disable()
+      .authorizeHttpRequests(authorize ->
+        authorize
+          .requestMatchers("/admin/**")
+          .hasRole("ADMIN")
+          .requestMatchers("/member/**")
+          .hasAnyRole("ADMIN", "MEMBER")
+          .anyRequest()
+          .permitAll()
+      )
+      .formLogin()
+      .loginPage("/user/login") //GET
+      .loginProcessingUrl("/user/login") //post
+      .defaultSuccessUrl("/index")
+      .and()
+      .logout()
+      .logoutUrl("/user/logout")
+      .logoutSuccessUrl("/");
+
+    return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry
+      .addInterceptor(singleVisitInterceptor)
+      .addPathPatterns("/user/login/**");
+  }
 }
